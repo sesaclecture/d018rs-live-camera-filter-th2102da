7d247bf2581e3c3b8f0c0d74cbe101d003289056
김태현 <th2102da@gmail.com>
2025-09-18 18:06:11 +0900

    과제제제출

--- src/filters.py
+++ src/filters.py
@@ -5,24 +5,49 @@ import numpy as np
 class Filters:
     # TODO: Image kernels
     Kernels = {
+        "original": np.array(
+        [[0, 0, 0], [0, 1, 0], [0, 0, 0]], dtype=np.float32),
+        "blur": np.array(
+        [[1, 1, 1], [1, 1, 1], [1, 1, 1]], dtype=np.float32) / 9,
+        "gaussian blur": np.array(
+        [[1, 2, 1], [2, 4, 2], [1, 2, 1]], dtype=np.float32) / 16,
+        "sharpen": np.array(
+        [[0, -1, 0], [-1, 5, -1], [0, -1, 0]], dtype=np.float32),
+        "sobel (x)": np.array(
+        [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], dtype=np.float32),
+        "sobel (y)": np.array(
+        [[-1, -2, -1], [0, 0, 0], [1, 2, 1]], dtype=np.float32),
+        "edge detection": np.array(
+        [[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]], dtype=np.float32),
+        "emboss":np.array(
+        [[-2, -1, 0], [-1, 1, 1], [0, 1, 2]], dtype=np.float32)
     }
 
     def __init__(self, kernels=Kernels):
         self.kernels = kernels
         # TODO: Implement internal variables
+        self.keys = list(self.kernels.keys())
+        self.current_index = 0
+        
+
+
+
 
     def apply_filter(self, frame, filter_name) -> np.array:
         # TODO: Apply the selected filter kernel to the frame
-        pass
+        kernel = self.kernels[filter_name]
+        return cv2.filter2D(frame, -1, kernel)
 
     def get_current_filter_name(self) -> str:
         # TODO: Return currently set kernels's name
-        pass
+        return self.keys[self.current_index]
 
     def switch_next_filter(self):
         # TODO: Update currently selected kernel to the next
-        pass
-
+        
+        self.current_index = (self.current_index+1) % len(self.keys)
+        print(self.current_index)
     def switch_previous_filter(self):
         # TODO: Update currently selected kernel to the previous
-        pass
+        self.current_index = (self.current_index-1) % len(self.keys)
+        print(self.current_index)
